1840345f5e85f335cd270def33fbac569618c8b3
JumanaNehad <96887669+JumanaNehad@users.noreply.github.com>
2022-01-22 05:40:54 +0200

    Add files via upload

---
+++ errorHandling.php
@@ -0,0 +1,20 @@
+<style>
+h5.error{
+    position: relative;
+   top:350px;
+    left:600px;
+    color : blue;
+}
+</style>
+<?php
+
+function customError($errno,$errmsg,$errfile,$errline)
+{
+$message=" Error no:".$errno." Error Message:".$errmsg." File name: " .$errfile." Line:".$errline;
+error_log($message,3,"error_log.txt");
+?> <h5 class="error"> <?php die("Fatal error , cant proceed.");?></h5>
+
+<?php
+}
+set_error_handler("customError");
+?>
